c58e27c0eee91bedc026fde586b052a3c3ba7af9
rgarciap <rjgpinel@gmail.com>
2021-10-13 13:54:26 +0200

    agent: format agents

--- mime/agent/script_agent.py
+++ mime/agent/script_agent.py
@@ -28,7 +28,8 @@ class ScriptAgent(Agent):
         else:
             self._steps = itertools.chain(*script)
 
-    '''Pre-compute actions at each new script'''
+    """Pre-compute actions at each new script"""
+
     def get_action_update(self):
         action = next(self._steps, None)
         if action is None and self._multiple_scripts:
@@ -37,21 +38,27 @@ class ScriptAgent(Agent):
 
         return action
 
+
 def make_noised(action):
-    if 'joint_velocity' in action:
-        action['joint_velocity'] += np.random.normal(scale=0.01, size=6)
-    if 'linear_velocity' in action:
-        action['linear_velocity'] += np.random.normal(scale=0.007, size=3)
-    if 'angular_velocity' in action:
-        action['angular_velocity'] += np.random.normal(scale=0.04, size=3)
+    if "joint_velocity" in action:
+        action["joint_velocity"] += np.random.normal(scale=0.01, size=6)
+    if "linear_velocity" in action:
+        action["linear_velocity"] += np.random.normal(scale=0.007, size=3)
+    if "angular_velocity" in action:
+        action["angular_velocity"] += np.random.normal(scale=0.04, size=3)
 
 
-@click.command(help='script_agent env_name [options]')
-@click.argument('env_name', type=str)
-@click.option('-s', '--seed', default=0, help='seed')
-@click.option('-t', '--times-repeat', default=1, help='times to repeat the script')
-@click.option('-n', '--add-noise', is_flag=True, help='adding noise to actions or not')
-@click.option('-sc', '--skill-collection/--no-skill-collection', is_flag=True, help='whether to show the skills collection')
+@click.command(help="script_agent env_name [options]")
+@click.argument("env_name", type=str)
+@click.option("-s", "--seed", default=0, help="seed")
+@click.option("-t", "--times-repeat", default=1, help="times to repeat the script")
+@click.option("-n", "--add-noise", is_flag=True, help="adding noise to actions or not")
+@click.option(
+    "-sc",
+    "--skill-collection/--no-skill-collection",
+    is_flag=True,
+    help="whether to show the skills collection",
+)
 def main(env_name, seed, times_repeat, add_noise, skill_collection):
     env = gym.make(env_name)
     scene = env.unwrapped.scene
@@ -73,17 +80,21 @@ def main(env_name, seed, times_repeat, add_noise, skill_collection):
         frames = []
 
         while not done and action is not None:
+            print(action)
+            print(obs["grip_velocity"])
             obs, reward, done, info = env.step(action)
             action = agent.get_action()
             if add_noise and action is not None:
                 make_noised(action)
 
         if action is None:
-            info['failure_message'] = 'End of Script.'
-        if not info['success']:
-            click.secho('Failure Seed {}: {}'.format(seed, info['failure_message']), fg='red')
+            info["failure_message"] = "End of Script."
+        if not info["success"]:
+            click.secho(
+                "Failure Seed {}: {}".format(seed, info["failure_message"]), fg="red"
+            )
 
-        print('Success', info['success'])
+        print("Success", info["success"])
 
 
 if __name__ == "__main__":
