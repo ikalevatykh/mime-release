e03005ade976b26768e2f3ad72b5ee748cd03593
rgarciap <rjgpinel@gmail.com>
2021-11-02 17:37:07 +0100

    table_envs/tower: update

--- mime/envs/table_envs/tower_scene.py
+++ mime/envs/table_envs/tower_scene.py
@@ -15,17 +15,23 @@ class TowerScene(TableScene):
         self._num_cubes = 2
         self._cubes = []
         self._cubes_size = []
-        self._cube_size_range = {'low': 0.03, 'high': 0.085}
+        self._cube_size_range = {"low": 0.03, "high": 0.085}
 
         v, w = self._max_tool_velocity
         self._max_tool_velocity = (1.5 * v, w)
 
-    def load(self):
-        super(TowerScene, self).load()
+    def load(self, np_random):
+        super(TowerScene, self).load(np_random)
 
     def reset(self, np_random):
         super(TowerScene, self).reset(np_random)
         modder = self._modder
+
+        # load and randomize cage
+        modder.load_cage(np_random)
+        if self._domain_rand:
+            modder.randomize_cage_visual(np_random)
+
         self._count_success = 0
 
         low, high = self._workspace
@@ -35,8 +41,6 @@ class TowerScene(TableScene):
         low_cubes[:2] += 0.02
         high_cubes[:2] -= 0.02
 
-        if self._cage is not None:
-            self._cage.remove()
         for cube in self._cubes:
             cube.remove()
         self._cubes = []
@@ -45,16 +49,12 @@ class TowerScene(TableScene):
         init_qpos = self._lab_init_qpos
         self.robot.arm.reset(init_qpos)
 
-        # load and set cage to a random position
-        modder.load_cage(np_random)
-
         # load cubes
         num_cubes = self._num_cubes
         cubes = []
         cubes_size = []
         for i in range(num_cubes):
-            cube, cube_size = modder.load_mesh(
-                'cube', self._cube_size_range, np_random)
+            cube, cube_size = modder.load_mesh("cube", self._cube_size_range, np_random)
             # cube.dynamics.lateral_friction = 10
             cubes.append(cube)
             cubes_size.append(cube_size)
@@ -70,8 +70,9 @@ class TowerScene(TableScene):
         cubes = []
         aabbs = []
         for cube in self._cubes:
-            aabbs, _ = sample_without_overlap(cube, aabbs, np_random,
-                    low_cubes, high_cubes, 0, 0, min_dist=0.05)
+            aabbs, _ = sample_without_overlap(
+                cube, aabbs, np_random, low_cubes, high_cubes, 0, 0, min_dist=0.05
+            )
             color = np_random.uniform(0, 1, 4)
             color[3] = 1
             cube.color = color
@@ -93,10 +94,11 @@ class TowerScene(TableScene):
                 sc.tool_move(arm, pick_pos + [0, 0, 0.1]),
                 sc.tool_move(arm, pick_pos + [0, 0, z_offset]),
                 sc.grip_close(grip),
-                sc.tool_move(arm, pick_pos + [0, 0, height+z_offset+0.02]),
-                sc.tool_move(arm, tower_pos + [0, 0, height+z_offset+0.02]),
+                sc.tool_move(arm, pick_pos + [0, 0, height + z_offset * 2]),
+                sc.tool_move(arm, tower_pos + [0, 0, height + z_offset * 2]),
                 sc.grip_open(grip),
-                sc.tool_move(arm, tower_pos + [0, 0, height+cube_size]),]
+                sc.tool_move(arm, tower_pos + [0, 0, height + cube_size]),
+            ]
 
         return moves
 
@@ -106,7 +108,9 @@ class TowerScene(TableScene):
 
     def distance_to_cubes(self, idx):
         tool_pos, _ = self.robot.arm.tool.state.position
-        return np.array([np.subtract(cube.position[0], tool_pos) for cube in self._cubes])
+        return np.array(
+            [np.subtract(cube.position[0], tool_pos) for cube in self._cubes]
+        )
 
     def get_reward(self, action):
         return 0
@@ -117,19 +121,25 @@ class TowerScene(TableScene):
         tower_pos = cubes_pos[0]
         heights = np.cumsum(cubes_size)[1:]
         bary_cubes = np.mean(cubes_pos[1:], axis=0)
-        cubes_on_tower = np.linalg.norm(np.subtract(tower_pos[:2], bary_cubes[:2])) < 0.04
+        cubes_on_tower = (
+            np.linalg.norm(np.subtract(tower_pos[:2], bary_cubes[:2])) < 0.04
+        )
 
         heights_ok = True
         for cube_pos, cube_size, height in zip(cubes_pos[1:], cubes_size[1:], heights):
-            heights_ok = heights_ok and np.abs(cube_pos[2]+cube_size/2-height) < 0.001
+            heights_ok = (
+                heights_ok and np.abs(cube_pos[2] + cube_size / 2 - height) < 0.001
+            )
 
         if heights_ok and cubes_on_tower:
             self._count_success += 1
         success = self._count_success > 5
         return success
 
+
 def test_scene():
     from time import sleep
+
     scene = TowerScene()
     scene.renders(True)
     np_random = np.random.RandomState(1)
